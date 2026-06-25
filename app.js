feed46e45003ee3c9058e17b32723ae57ce05dca
Abufauzan1206 <ababdussalam1206@gmail.com>
2026-06-25 15:25:19 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,35 +1,7 @@
-const MONTHS = [
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
-  "January",
-  "February",
-  "March"
-];
-
-const app = firebaseModules.initializeApp(firebaseConfig);
-const auth = firebaseModules.getAuth(app);
-const db = firebaseModules.getFirestore(app);
+alert("app.js loaded successfully");
 
 const googleBtn = document.getElementById("googleBtn");
-const agreeBox = document.getElementById("agreeBox");
-const userSection = document.getElementById("userSection");
 
-let currentUser = null;
-
-googleBtn.addEventListener("click", async () => {
-
-  if (!agreeBox.checked) {
-    alert("You must agree to the Terms and Conditions.");
-    return;
-  }
-
-  alert(
-    "Google Sign-In logic will be added in the next step."
-  );
-
+googleBtn.addEventListener("click", () => {
+  alert("Verify button clicked");
 });
