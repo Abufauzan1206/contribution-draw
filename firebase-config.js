cfb534102e781182b64e157d8dd1f0b09b9dc304
Abufauzan1206 <ababdussalam1206@gmail.com>
2026-06-25 05:58:01 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,14 @@
+// Firebase configuration
 
+const firebaseConfig = {
+  apiKey: "AIzaSyChppUrsnFVpzz5gsYj5CaOp1-XM-ps0Yo",
+  authDomain: "contribution-draw.firebaseapp.com",
+  projectId: "contribution-draw",
+  storageBucket: "contribution-draw.firebasestorage.app",
+  messagingSenderId: "979502527071",
+  appId: "1:979502527071:web:3a0f546b1cab8ae23161ea",
+  measurementId: "G-70F2W982NF"
+};
+
+// Admin email
+const ADMIN_EMAIL = "ababdussalam1206@gmail.com";
