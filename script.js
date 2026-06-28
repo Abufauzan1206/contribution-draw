0004ed1faa20dee1d99e8549a6aff173d5a38a73
Abufauzan1206 <ababdussalam1206@gmail.com>
2026-06-28 10:38:54 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 alert("Contribution Draw starting");
 
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import {
   getAuth,
   GoogleAuthProvider,
@@ -8,7 +7,6 @@ import {
   onAuthStateChanged
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
-const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
 const googleBtn = document.getElementById("googleBtn");
